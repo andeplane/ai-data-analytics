2fe7fa859f3fd4e1f706d736ceb00c2145508e60
Anders Hafreager <anders.hafreager@cognite.com>
2026-01-01 11:48:42 +0100

    chore: add TypeScript global types and config updates

---
+++ src/global.d.ts
@@ -0,0 +1,16 @@
+// Fix for react-markdown JSX namespace issue with React 19
+// See: https://github.com/remarkjs/react-markdown/issues/772
+import type { JSX as ReactJSX } from 'react'
+
+declare global {
+  namespace JSX {
+    interface Element extends ReactJSX.Element {}
+    interface ElementClass extends ReactJSX.ElementClass {}
+    interface ElementAttributesProperty extends ReactJSX.ElementAttributesProperty {}
+    interface ElementChildrenAttribute extends ReactJSX.ElementChildrenAttribute {}
+    interface IntrinsicAttributes extends ReactJSX.IntrinsicAttributes {}
+    interface IntrinsicClassAttributes<T> extends ReactJSX.IntrinsicClassAttributes<T> {}
+    interface IntrinsicElements extends ReactJSX.IntrinsicElements {}
+  }
+}
+
